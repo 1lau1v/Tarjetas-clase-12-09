dcc75aa7b8ccde8f4d2553f5be0df323017fc732
1lau1v <105442044+1lau1v@users.noreply.github.com>
2022-09-12 16:10:13 -0500

    Add files via upload

---
+++ Ejercicio-clase/data/data.js
@@ -0,0 +1,33 @@
+export const data =
+[
+    {
+        "id":1,
+        "name":"P.A.E",
+        "image":"./imge/P.A.E.jpg"
+    },
+
+    {
+        "id":2,
+        "name":"Restaurante",
+        "image":"./imge/Restaurante.jpg"
+    },
+
+    {
+        "id":3,
+        "name":"beneficiarios",
+        "image":"./imge/beneficiarios.jpg"
+    },
+    
+    {
+        "id":4,
+        "name":"almuerzos",
+        "image":"./imge/almuerzos.jpg"
+    },
+    
+    {
+        "id":5,
+        "name":"comedores_escolares",
+        "image":"./imge/comedores_escolares.jpg"
+    },
+    
+]
